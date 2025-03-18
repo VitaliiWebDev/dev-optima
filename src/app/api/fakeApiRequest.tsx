1df10bc00e6481b74a2c660a43159aea0fa2c9e0
Vitlike <vitllike@yandex.ru>
2025-03-18 23:23:56 +0100

    fix: 'email' is defined but never used

--- src/app/api/fakeApiRequest.tsx
+++ src/app/api/fakeApiRequest.tsx
@@ -2,7 +2,7 @@ const fakeApiRequest = (email: string) => {
   return new Promise<string>((resolve, reject) => {
     setTimeout(() => {
       if (Math.random() > 0.5) {
-        resolve("Send success");
+        resolve(email + " Send success");
       } else {
         reject("Send error. Try again later");
       }
