2105ec4e7c76e6e43033157cbd696e46104d8021
Vitlike <vitllike@yandex.ru>
2025-03-26 17:08:00 +0100

    refactor: changed connection of elements in layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,8 @@
 import type { Metadata } from "next";
 
+import Header from "@/components/pages/home/header";
+import Footer from "@/components/pages/home/footer/footer";
+
 import { aeonik, inter } from "./fonts";
 
 import "./globals.css";
@@ -17,7 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en" className={`${aeonik.variable} ${inter.variable}`}>
       <body>
-        <div className="overflow-hidden">{children}</div>
+        <div className="overflow-hidden relative flex min-h-screen flex-col">
+          <Header />
+          <main>{children}</main>
+          <Footer />
+        </div>
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,17 @@
-import Header from "../components/pages/home/header";
-import Hero from "../components/pages/home/hero";
-import About from "../components/pages/home/about";
-import Innovative from "../components/pages/home/innovative";
-import Testimonials from "../components/pages/home/testimonials";
-import Ready from "../components/pages/home/ready";
-import Footer from "../components/pages/home/footer/footer";
+import Hero from "@/components/pages/home/hero";
+import About from "@/components/pages/home/about";
+import Innovative from "@/components/pages/home/innovative";
+import Testimonials from "@/components/pages/home/testimonials";
+import Ready from "@/components/pages/home/ready";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <main>
-        <Hero />
-        <About />
-        <Innovative />
-        <Testimonials />
-        <Ready />
-        <Footer />
-      </main>
+      <Hero />
+      <About />
+      <Innovative />
+      <Testimonials />
+      <Ready />
     </>
   );
 }
